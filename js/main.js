b42e34050bef40db75d52a0aa5164880ea94c50d
divyasowndappan1902 <divyasowndappan1902@gmail.com>
2026-08-31 20:10:22 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -28,6 +28,8 @@ document.addEventListener('DOMContentLoaded', () => {
     if (mobileToggle && mainNav) {
         mobileToggle.addEventListener('click', () => {
             mainNav.classList.toggle('active');
+            document.body.classList.toggle('no-scroll');
+            document.documentElement.classList.toggle('no-scroll');
             
             // Toggle icon
             const icon = mobileToggle.querySelector('i');
